3942708632d9a37347424375d47620293996cbec
Julio Falbo <jfa@inpay.com>
2020-06-19 23:07:10 +0200

    Adding init script to DB, Start NiFi-Registry using ROOT user, fixing NiFi-Registry volumes

---
+++ postgres/init/01_create_db_and_table_person.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE nifi;
+USE nifi;
+
+create table person (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20),
+    ctime VARCHAR(55)
+);
