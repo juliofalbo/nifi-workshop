2f53c08d3ba02c84e869d92c1234324260c0ae78
Julio Falbo <jfa@inpay.com>
2020-09-26 21:24:32 +0200

    Adding Custom Processor

---
+++ nifi/custom-processor/installCustomProcessor.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd "$BASEDIR/CustomProcessor" || exit;
+
+mvn clean install
+
+cp "$BASEDIR/CustomProcessor/nifi-custom-nar/target/nifi-custom-nar-1.0.nar" "$BASEDIR/../libs"
